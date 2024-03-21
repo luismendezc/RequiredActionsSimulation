a0646555a227cad1d9d8e45e030b53fe3c50f1a7
Luis Mendez <luismendezescobar@gmail.com>
2024-03-20 19:00:58 -0600

    adding changes to build.gradle.kts file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     applicationId = "com.oceloti.lemc.requiredactionssimulation"
     minSdk = 28
     targetSdk = 33
-    versionCode = 3
+    versionCode = 4
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
