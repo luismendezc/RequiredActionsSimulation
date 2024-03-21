22db31bc9ae71005874dd877191cb810b9145ed6
Luis Mendez <luismendezescobar@gmail.com>
2024-03-20 18:59:20 -0600

    changing the version to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     applicationId = "com.oceloti.lemc.requiredactionssimulation"
     minSdk = 28
     targetSdk = 33
-    versionCode = 2
+    versionCode = 3
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
